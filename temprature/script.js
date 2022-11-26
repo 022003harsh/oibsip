c9e558918f1dba9848e89dafb1e27f70ca10eefa
HARSH RAJ <96056641+022003harsh@users.noreply.github.com>
2022-11-27 02:03:56 +0530

    Add files via upload

---
+++ temprature/script.js
@@ -0,0 +1,33 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+
+function celToFar() {
+    let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function farToCel() {
+    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+    console.log(output);
+}
+        
+
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
